9ffef5022a036acff3714f7317ff7958ef04b90a
Sandor Torok <santor@users.noreply.github.com>
2020-12-02 19:13:07 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Arithmetic title="this is a static title" />
+</template>
+
+<script>
+  import Arithmetic from './components/Arithmetic.vue';
+
+  export default {
+    name: 'App',
+
+    components: {
+      Arithmetic,
+    },
+  };
+  // const app = {}
+
+  // export { app };
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: {
+    content: ['./public/**/*.html', './src/**/*.vue'],
+    options: {
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+      ],
+    },
+  },
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      textDecoration: ['focus-visible'],
+      backgroundColor: ['active'],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('tailwindcss-scroll-snap'),
+  ],
+};
